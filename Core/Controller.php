cd8a08be0a97f8b64f26315fbba10c053c8d9ecc
Abdulhamid Alelaiyan <quickisslow191@gmail.com>
2019-09-06 19:35:31 +0300

    Added Info logging for the controller

--- Core/Controller.php
+++ Core/Controller.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Psr7\Stream;
 use Monolog\Formatter\HtmlFormatter;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
 use Monolog\Processor\MemoryUsageProcessor;
 use Monolog\Processor\PsrLogMessageProcessor;
 use Monolog\Processor\UidProcessor;
@@ -31,7 +32,7 @@ abstract class Controller
      * Logger for the controller
      * @var Logger
      */
-//    protected $logger;
+    protected $logger;
 
     /**
      * Class constructor
@@ -45,25 +46,51 @@ abstract class Controller
         $this->route_params = $route_params;
     }
 
-	/**
-	 * Magic method called when a non-existent or inaccessible method is
-	 * called on an object of this class. Used to execute before and after
-	 * filter methods on action methods. Action methods need to be named
-	 * with an "Action" suffix, e.g. indexAction, showAction etc.
-	 *
-	 * @param string $name Method name
-	 * @param array $args Arguments passed to the method
-	 *
-	 * @return void
-	 */
+    /**
+     * Magic method called when a non-existent or inaccessible method is
+     * called on an object of this class. Used to execute before and after
+     * filter methods on action methods. Action methods need to be named
+     * with an "Action" suffix, e.g. indexAction, showAction etc.
+     *
+     * @param string $name Method name
+     * @param array $args Arguments passed to the method
+     *
+     * @return void
+     * @throws \Exception
+     */
     public function __call($name, $args)
     {
         $method = $name . 'Action';
 
         if (method_exists($this, $method)) {
+            // Set the logger
+            $this->logger = new Logger('Controller');
+            $fileHandler = new StreamHandler(dirname(__DIR__) . '/logs/INFO.html', Logger::INFO);
+            $fileHandler->setFormatter(new HtmlFormatter());
+            $this->logger->pushHandler($fileHandler);
+            $this->logger->pushProcessor(new WebProcessor());
+            $this->logger->pushProcessor(new UidProcessor());
+            $this->logger->pushProcessor(new MemoryUsageProcessor());
+            $this->logger->pushProcessor(new PsrLogMessageProcessor());
+            $this->logger->pushProcessor(new IntrospectionProcessor());
+            $user = Auth::getUser();
+            if($user)
+            {
+                $userEmail = $user->email;
+            }
+            else
+            {
+                $userEmail = 'No User Session';
+            }
+            $this->logger->addInfo($method . ' action was requested', ['user-email' => $userEmail]); // TODO: add the arguments in the log message.
+
+            // Call the action filters
             if ($this->before() !== false) {
+                $this->logger->addInfo($method . ' action filter (before) passed', ['user-email' => $userEmail]);
                 call_user_func_array([$this, $method], $args);
+                $this->logger->addInfo($method . ' action executed', ['user-email' => $userEmail]);
                 $this->after();
+                $this->logger->addInfo($method . ' action filter (after) passed', ['user-email' => $userEmail]);
             }
         } else {
             throw new \Exception("Method $method not found in controller " . get_class($this));
