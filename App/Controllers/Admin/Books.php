87a74d4a07795c316960ad5652ff10620d8cd301
Abdulhamid Alelaiyan <quickisslow191@gmail.com>
2019-09-09 20:11:43 +0300

    Admin now are able to add new books information.

--- App/Controllers/Admin/Books.php
+++ App/Controllers/Admin/Books.php
@@ -2,12 +2,55 @@
 
 namespace App\Controllers\Admin;
 
+use App\Models\Book;
 use \Core\View;
 
 class Books extends AdminController
 {
-    protected function indexAction()
+    public function indexAction()
     {
         echo 'Admin namespace, Books controller, index action';
+    }
+
+
+    /**
+     * Load the view of new book form
+     *
+     * @return void
+     */
+    public function newAction()
+    {
+        View::renderTemplate('Admin/Books/new.html.twig');
+    }
+
+    /**
+     * Add the new book to the database
+     *
+     * @return void
+     */
+    public function createAction()
+    {
+        $newBook = new Book($_POST);
+        if($newBook->save())
+        {
+            $this->redirect('/admin/books/add-success');
+        }
+        else
+        {
+            View::renderTemplate('Admin/Books/new.html.twig',
+                [
+                    'errors' => $newBook->getErrors(),
+                ]);
+        }
+    }
+
+    /**
+     * Load the add-success view
+     *
+     * @return void
+     */
+    public function addSuccessAction()
+    {
+        View::renderTemplate('Admin/Books/add-success.html.twig');
     }
 }
--- App/Models/Book.php
+++ App/Models/Book.php
@@ -8,32 +8,105 @@ use \PDO;
 
 class Book extends \Core\Model
 {
-	/**
-	 * Get all books for a single page
-	 *
-	 * @param int $book_per_page
-	 * @return array
-	 */
-	public static function getBooks(int $book_per_page = 3): array
-	{
-		// get the DB connection
-		$db = static::getDB();
-		// get the Zebra paginator
-		$pg = new \Zebra_Pagination();
 
-		$total_books_sql = "SELECT count(*) AS count FROM books";
-		$current_page_books_sql = "SELECT title, author FROM books 
-LIMIT " . ($pg->get_page() - 1) * $book_per_page . ", $book_per_page";
+    /**
+     * errors array, contains validation errors
+     * @var array
+     */
+    protected $errors;
 
-		$stmt = $db->query($total_books_sql);
-		$total_books = $stmt->fetch()['count'];
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
 
-		$stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
-		$current_page_books = $db->query($current_page_books_sql);
+    /**
+     * Constructor for the Book model
+     *
+     */
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        };
+    }
 
-		$pg->records_per_page($book_per_page);
-		$pg->records($total_books);
+    /**
+     * Validation of the data fed to the Book object, if any error it will be stored in $errors array
+     *
+     * @return void
+     */
+    protected function validate()
+    {
+        if(!isset($this->title) || empty($this->title))
+        {
+            $this->errors[] = 'Title must be provided.';
+        }
+        else
+        {
+            $this->title = filter_var($this->title, FILTER_SANITIZE_STRING);
+        }
 
-		return ['links' => $pg->render(true), 'books' => $current_page_books];
-	}
+        if(!isset($this->authors) || empty($this->authors))
+        {
+            $this->errors[] = 'Author/s name/s must be provided.';
+        }
+        else
+        {
+            $this->authors = filter_var($this->authors, FILTER_SANITIZE_STRING);
+        }
+
+        if(!isset($this->isbn) || empty($this->isbn) || strlen($this->isbn) != 10 ||
+            !filter_var($this->isbn, FILTER_VALIDATE_INT))
+        // TODO: Check if the book is already added
+        {
+            $this->errors[] = 'ISBN must be provided and must be 10 numbers.';
+        }
+
+        if(!isset($this->edition) || empty($this->isbn))
+        {
+            $this->errors[] = 'Edition must be provided.';
+        }
+
+        if(!isset($this->date) || empty($this->date))
+        {
+            $this->errors[] = 'Publication Date must be provided.';
+        }
+        else
+        {
+            $this->date = strtotime($this->date);
+            $this->date = date('Y-m-d');
+        }
+    }
+
+    /**
+     * Save the current instance to the database.
+     *
+     * @return boolean True if stored in the database, false otherwise
+     */
+    public function save()
+    {
+        $this->validate();
+
+        if(is_null($this->errors))
+        {
+            $db = static::getDB();
+            $sql = 'INSERT INTO books_information (title, isbn, edition, publication_date, authors) VALUES (:title, :isbn,
+                                                                               :edition, :publication_date, :authors)';
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':isbn', $this->isbn, PDO::PARAM_STR);
+            $stmt->bindValue(':edition', $this->edition, PDO::PARAM_STR);
+            $stmt->bindValue(':publication_date', $this->date, PDO::PARAM_STR);
+            $stmt->bindValue(':authors', $this->authors, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+
+        return false;
+    }
 }
