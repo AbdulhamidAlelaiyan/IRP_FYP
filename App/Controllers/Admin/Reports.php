08d9e24f557f291bbf657e7fcf828e4b248c4be0
Abdulhamid Alelaiyan <quickisslow191@gmail.com>
2019-09-26 21:39:29 +0300

    Reports can be deleted and viewed by the admin in the admin dashboard

--- App/Controllers/Admin/Reports.php
+++ App/Controllers/Admin/Reports.php
@@ -2,7 +2,110 @@
 
 namespace App\Controllers\Admin;
 
+use App\Flash;
+use App\Models\Report;
+use Core\View;
+
 class Reports extends AdminController
 {
+    /**
+     * List all reports of posts sorted by the latest
+     *
+     * @return void
+     */
+    public function indexPostsAction()
+    {
+        $reports = Report::getAllPostReports();
+        View::renderTemplate('Admin/Reports/index-posts.html.twig',
+            [
+                'reports' => $reports,
+            ]);
+    }
+
+    /**
+     * List all reports of posts sorted by the latest
+     *
+     * @return void
+     */
+    public function indexRepliesAction()
+    {
+        $reports = Report::getAllRepliesReports();
+        View::renderTemplate('Admin/Reports/index-replies.html.twig',
+            [
+                'reports' => $reports,
+            ]);
+    }
+
+    /**
+     * Delete post report
+     *
+     * @return void
+     */
+    public function deletePost()
+    {
+        $report_id = filter_var($this->route_params['isbn'], FILTER_SANITIZE_NUMBER_INT);
+        if($report_id)
+        {
+            if($report = Report::getPostReportByID($report_id))
+            {
+                if($report->deletePostReport())
+                {
+                    Flash::addMessage('Report deleted!', Flash::SUCCESS);
+                    $this->redirect('/admin/reports/index-posts');
+                }
+                else
+                {
+                    Flash::addMessage('Report NOT deleted!', Flash::DANGER);
+                    $this->redirect('/admin/reports/index-posts');
+                }
+            }
+            else
+            {
+                Flash::addMessage('Report NOT found!', Flash::DANGER);
+                $this->redirect('/admin/reports/index-posts');
+            }
+        }
+        else
+        {
+            Flash::addMessage('Report ID NOT found!', Flash::DANGER);
+            $this->redirect('/admin/reports/index-posts');
+        }
+    }
+
+    /**
+     * Delete reply report
+     *
+     * @return void
+     */
+    public function deleteReply()
+    {
+        $report_id = filter_var($this->route_params['isbn'], FILTER_SANITIZE_NUMBER_INT);
+        if($report_id)
+        {
+            if($report = Report::getReplyReportByID($report_id))
+            {
+                if($report->deleteReplyReport())
+                {
+                    Flash::addMessage('Report deleted!', Flash::SUCCESS);
+                    $this->redirect('/admin/reports/index-replies');
+                }
+                else
+                {
+                    Flash::addMessage('Report NOT deleted!', Flash::DANGER);
+                    $this->redirect('/admin/reports/index-replies');
+                }
+            }
+            else
+            {
+                Flash::addMessage('Report NOT found!', Flash::DANGER);
+                $this->redirect('/admin/reports/index-replies');
+            }
+        }
+        else
+        {
+            Flash::addMessage('Report ID NOT found!', Flash::DANGER);
+            $this->redirect('/admin/reports/index-replies');
+        }
+    }
 
 }
--- App/Models/Report.php
+++ App/Models/Report.php
@@ -46,4 +46,127 @@ class Report extends \Core\Model
         $stmt->bindValue(':text', $text, PDO::PARAM_STR);
         return $stmt->execute();
     }
+
+    /**
+     * Get all the reports of posts in the database
+     *
+     * @return array of all the reports
+     */
+    public static function getAllPostReports()
+    {
+        $db = static::getDB();
+        $sql = 'SELECT * FROM posts_reports';
+        $stmt = $db->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+        $reports = $stmt->fetchAll();
+        foreach($reports as $report)
+        {
+            $user = User::findByID($report->user_id);
+            $username = $user->name;
+            $report->username = $username;
+            $post = Post::getPostByID($report->post_id);
+            $post_title = $post->title;
+            $report->post_title = $post_title;
+        }
+        return $reports;
+    }
+
+    /**
+     * Get all the reports of posts in the database
+     *
+     * @return array of all the reports
+     */
+    public static function getAllRepliesReports()
+    {
+        $db = static::getDB();
+        $sql = 'SELECT * FROM replies_reports';
+        $stmt = $db->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+        $reports = $stmt->fetchAll();
+        foreach($reports as $report)
+        {
+            $user = User::findByID($report->user_id);
+            $username = $user->name;
+            $report->username = $username;
+        }
+        return $reports;
+    }
+
+    /**
+     * Return a post report of a given ID
+     *
+     * @param int $id The ID of the report
+     *
+     * @return mixed Report with ID of $id, False otherwise
+     */
+    public static function getPostReportByID($id)
+    {
+        $db = static::getDB();
+        $sql = 'SELECT * FROM posts_reports WHERE id = :id LIMIT 1';
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        if($stmt->execute())
+        {
+            return $stmt->fetch();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Delete the current instance of the post report from the posts_reports table in the database
+     *
+     * @return boolean True if deleted, False otherwise
+     */
+    public function deletePostReport()
+    {
+        $db = static::getDB();
+        $sql = 'DELETE FROM posts_reports WHERE id = :id';
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $this->id,PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    /**
+     * Return a post report of a given ID
+     *
+     * @param int $id The ID of the report
+     *
+     * @return mixed Report with ID of $id, False otherwise
+     */
+    public static function getReplyReportByID($id)
+    {
+        $db = static::getDB();
+        $sql = 'SELECT * FROM replies_reports WHERE id = :id LIMIT 1';
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        if($stmt->execute())
+        {
+            return $stmt->fetch();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Delete the current instance of the reply report from the posts_reports table in the database
+     *
+     * @return boolean True if deleted, False otherwise
+     */
+    public function deleteReplyReport()
+    {
+        $db = static::getDB();
+        $sql = 'DELETE FROM replies_reports WHERE id = :id';
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $this->id,PDO::PARAM_INT);
+        return $stmt->execute();
+    }
 }
